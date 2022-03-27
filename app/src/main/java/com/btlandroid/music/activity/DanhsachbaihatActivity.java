d19f4415cd1919371a9c3d4f80b6a35f58b041a1
LinhMy2001 <linhmy.pui@gmail.com>
2022-03-27 15:44:53 +0700

    add ten bien cho activiy danh sach cacbaihat

--- app/src/main/java/com/btlandroid/music/activity/DanhsachbaihatActivity.java
+++ app/src/main/java/com/btlandroid/music/activity/DanhsachbaihatActivity.java
@@ -1,21 +1,40 @@
 package com.btlandroid.music.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.btlandroid.music.R;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DanhsachbaihatActivity extends AppCompatActivity {
 
+    CoordinatorLayout coordinatorLayout;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    Toolbar toolbar;
+    RecyclerView recyclerViewdanhsachbaihat;
+    FloatingActionButton floatingActionButton;
     Quangcao quangcao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danhsachbaihat);
         DateIntent();
+        anhxa();
+    }
+
+    private void anhxa() {
+        coordinatorLayout = findViewById(R.id.coordinatorlayout);
+        collapsingToolbarLayout = findViewById(R.id.collapsingtoolbar);
+        toolbar = findViewById(R.id.toolbardanhsach);
+        recyclerViewdanhsachbaihat= findViewById(R.id.recyclerviewdanhsachbaihat);
+        floatingActionButton= findViewById(R.id.floatingactionbutton);
     }
 
     private void DateIntent() {

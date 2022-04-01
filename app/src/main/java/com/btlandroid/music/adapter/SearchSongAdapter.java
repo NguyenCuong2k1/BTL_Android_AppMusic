1b2336d0dd5ad56131b573a20ea063674ef6c7e8
Cuong <ndcuong2k1@gmail.com>
2022-04-01 16:37:44 +0700

    Add model Song

---
+++ app/src/main/java/com/btlandroid/music/adapter/SearchSongAdapter.java
@@ -0,0 +1,115 @@
+package com.btlandroid.music.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.btlandroid.music.R;
+import com.btlandroid.music.activity.PlaySongActivity;
+import com.btlandroid.music.config.Config;
+import com.btlandroid.music.model.BaiHat;
+import com.btlandroid.music.service.APIService;
+import com.btlandroid.music.service.DataService;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SearchSongAdapter extends RecyclerView.Adapter<SearchSongAdapter.ViewHolder> {
+    Context context;
+    ArrayList<BaiHat> listSong;
+
+    public SearchSongAdapter(Context context, ArrayList<BaiHat> listSong) {
+        this.context = context;
+        this.listSong = listSong;
+    }
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_search_song, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        BaiHat song = listSong.get(position);
+
+        holder.tvNameSinger.setText(song.getSinger());
+        holder.tvNameSong.setText(song.getTenBaiHat());
+
+        Picasso.get().load(Config.domain + song.getImageBaiHat()).into(holder.imvSong);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listSong.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvNameSong, tvNameSinger;
+        ImageView imvSong, imvLike;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvNameSong = itemView.findViewById(R.id.tvNameSong);
+            tvNameSinger = itemView.findViewById(R.id.tvNameSinger);
+            imvSong = itemView.findViewById(R.id.imvSong);
+            imvLike = itemView.findViewById(R.id.imvLike);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, PlaySongActivity.class);
+                    intent.putExtra("song", listSong.get(getPosition()));
+                    context.startActivity(intent);
+                }
+            });
+
+            imvLike.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    imvLike.setImageResource(R.drawable.ic_like);
+                    DataService dataService = APIService.getService();
+                    Call<String> callback = dataService.updateCountLike("1", listSong.get(getPosition()).getIdBaiHat());
+                    callback.enqueue(new Callback<String>() {
+                        @Override
+                        public void onResponse(Call<String> call, Response<String> response) {
+                            String results = response.body();
+                            if(results.equals("success")){
+                                Toast.makeText(context, "Đã thích", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(context, "Lỗi", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<String> call, Throwable t) {
+
+                        }
+                    });
+                    imvLike.setEnabled(false);
+                }
+            });
+
+        }
+    }
+}
